aa9717fdf683383b304b958b1f2eb6f1a8968731
PioV1996 <gustavo_app@outlook.com>
2020-10-07 22:53:38 -0500

    Módulo información

--- app.rb
+++ app.rb
@@ -7,13 +7,34 @@ require 'twilio-ruby'
 set :bind, '0.0.0.0'
 
 # Definición de credenciales necesarias para consumir Apis de Twlio
-auth_token  = ENV['TWILIO_AUTH_TOKEN']
-account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token  = 
+account_sid = 
 
-# Método principal, donde se introduce al usuario al menú principal
+# Método principal, donde se introduce al usuario al menú principal, y da infromación acerca de Mr. Nodo
 get '/' do
     content_type 'text/xml'
     Twilio::TwiML::VoiceResponse.new do |respuesta|
-        respuesta.say(message: 'Bienvenido a Mister. Nodo, por favor escuche nuestro menú', voice: 'woman', language: 'es-MX')
+        respuesta.say(message: 'Bienvenido a Mister Nodo', voice: 'woman', language: 'es-MX')
+        respuesta.say(message: 'Mister Nodo ofrece servicios de instalación y configuración de sistemas operativos, ' + 
+        'así como la realización de mantenimiento correctivo y preventivo a equipos de cómputo, contamos con un equipo' +
+        ' de profesionales capacitados para resolver desde los problemas más comunes, hasta aquellos que requiere de una mayor' +
+        ' atención. Estamos a sus órdenes en un horario de 9 de la mañana a 6 de la tarde, de lunes a viernes. Por favor' + 
+        ' escuche nuestro menú', voice: 'woman', language: 'es-MX')
+        respuesta.redirect('/menu', method: 'get')
     end.to_s
+end
+
+get '/menu' do
+    content_type 'text/xml'
+    Twilio::TwiML::VoiceResponse.new do |respuesta|
+
+        respuesta.gather(numDigits: 1, action: '/eleccion', method: 'get') do |g|
+            g.say(message: 'Para hablar con un ejecutivo, marque 1, ', voice: 'woman', language: 'es-MX')
+        end
+        respuesta.redirect('/menu', method: 'get')
+    end.to_s
+end
+
+get '/elige' do
+    
 end
