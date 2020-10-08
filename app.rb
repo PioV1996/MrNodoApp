0dab2345cc56c15cb7e7197adec604ed39b98919
PioV1996 <gustavo_app@outlook.com>
2020-10-08 16:19:45 -0500

    primer if else

--- app.rb
+++ app.rb
@@ -24,17 +24,28 @@ get '/' do
     end.to_s
 end
 
+# Se define el método del menú, el cual define las opciones que el usuario puede elegir conforme las necesidades de dicho usuario
+# Se hace uso del verbo "gather", el cual lee el dígito ingresado por el usuario, y a su vez redirecciona a la ruta definida
+# en "action" dentro de sus parámetros
 get '/menu' do
     content_type 'text/xml'
     Twilio::TwiML::VoiceResponse.new do |respuesta|
-
         respuesta.gather(numDigits: 1, action: '/eleccion', method: 'get') do |g|
-            g.say(message: 'Para hablar con un ejecutivo, marque 1, ', voice: 'woman', language: 'es-MX')
+            g.say(message: 'Para hablar con un ejecutivo, marque 1 ', voice: 'woman', language: 'es-MX')
         end
         respuesta.redirect('/menu', method: 'get')
     end.to_s
 end
 
-get '/elige' do
-    
+# Se define el método para procesar la opción elegida por el usuario.
+# En caso de no ingresar una opción válida, se redirige de nuevo al menú para ingresar un número asignado a una acción determinada
+get '/eleccion' do
+    content_type 'text/xml'
+    if params['Digits']
+        
+    else
+        Twilio::TwiML::VoiceResponse.new do |respuesta|
+            respuesta.say(message: 'Opción inválida', voice: 'woman', language: 'es-MX')
+        end
+    end
 end
