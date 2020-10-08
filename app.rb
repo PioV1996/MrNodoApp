c1fe21032471dbbcea1f176b9710a8774313027d
PioV1996 <gustavo_app@outlook.com>
2020-10-07 21:36:11 -0500

    Bienvenida a App

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
+# Ingreso de las librerías necesarias para la funcionalidad
+# de la aplicación web.
 require 'sinatra'
+require 'twilio-ruby'
 
+# Se definen los orígenes (IPs) que el servidor puede procesar
 set :bind, '0.0.0.0'
 
+# Definición de credenciales necesarias para consumir Apis de Twlio
+auth_token  = ENV['TWILIO_AUTH_TOKEN']
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+
+# Método principal, donde se introduce al usuario al menú principal
 get '/' do
-    'Hola mundo'
+    Twilio::TwiML::VoiceResponse.new do |respuesta|
+        respuesta.say(message: 'Bienvenido a Misterr. Nodo, por favor escuche nuestro menú', voice: 'woman', language: 'es-MX')
+    end
 end
